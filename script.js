579465dd542efdd5f26a3ef6720599f5ec0ebba6
nishagupta912274 <nishagupta912274@gmail.com>
2026-05-30 10:23:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function sendMessage() {
+    let number = document.getElementById("number").value;
+    let message = document.getElementById("message").value;
 
+    let url = `https://wa.me/91${number}?text=${encodeURIComponent(message)}`;
+
+    window.open(url, "_blank");
+}
